1bace85c1c1d77b52a2b1a03bb3575727d187ec5
Richard Felipe <richardfelipewt@hotmail.com>
2023-06-15 16:00:06 -0300

    Adicionando os métodos PUT e Delete

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,25 +10,4 @@ public class HomeController : ControllerBase
 {
     [HttpGet("/")]
     public IActionResult Get() => Ok();
-    
-    [HttpGet("/videos/{id:int}")]
-    public async Task<IActionResult> GetbyId([FromServices] VideoContext context, [FromRoute] int id)
-    {
-        var video = await context.Videos.FirstOrDefaultAsync(video => video.Id == id);
-
-        if (video is null)
-            return NotFound();
-
-        return Ok(video);
-    }
-
-    [HttpPost("/videos")]
-    public async Task<IActionResult> Post([FromServices] VideoContext context, [FromBody] Video model)
-    {
-        await context.Videos.AddAsync(model);
-        await context.SaveChangesAsync();
-
-        return Created($"/videos/{model.Id}", model);
-    }
-    //[HttpPut("/")]
 }
--- Controllers/VideoController.cs
+++ Controllers/VideoController.cs
@@ -11,4 +11,55 @@ public class VideoController : ControllerBase
     [HttpGet("/videos")]
     public async Task<IActionResult> Get([FromServices] VideoContext context) 
         => Ok(await context.Videos.AsNoTracking().ToListAsync());
+    [HttpGet("/videos/{id:int}")]
+    public async Task<IActionResult> GetbyId([FromServices] VideoContext context, [FromRoute] int id)
+    {
+        var video = await context.Videos.FirstOrDefaultAsync(video => video.Id == id);
+
+        if (video is null)
+            return NotFound();
+
+        return Ok(video);
+    }
+
+    [HttpPost("/videos")]
+    public async Task<IActionResult> Post([FromServices] VideoContext context, [FromBody] Video model)
+    {
+        await context.Videos.AddAsync(model);
+        await context.SaveChangesAsync();
+
+        return Created($"/videos/{model.Id}", model);
+    }
+
+    [HttpPut("videos/{id:int}")]
+    public async Task<IActionResult> Put([FromServices] VideoContext context, [FromRoute] int id, [FromBody] Video model)
+    {
+        var video = await context.Videos.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (video is null)
+            return NotFound();
+
+        video.Titulo = model.Titulo;
+        video.descricao = model.descricao;
+        video.URL = model.URL;
+
+        context.Videos.Update(video);
+        await context.SaveChangesAsync();
+
+        return Ok(video);
+    }
+
+    [HttpDelete("videos/{id:int}")]
+    public async Task<IActionResult> Delete([FromServices] VideoContext context, [FromRoute] int id)
+    {
+        var video = await context.Videos.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (video is null)
+            return NotFound();
+
+        context.Videos.Remove(video);
+        await context.SaveChangesAsync();
+
+        return Ok();
+    }
 }
