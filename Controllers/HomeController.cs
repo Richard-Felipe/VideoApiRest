cbf1852861aa439b594300ba4e0033e083fb3934
Richard Felipe <richardfelipewt@hotmail.com>
2023-06-15 14:14:51 -0300

    Implementando o HomeController e o Método Get dentro da VideoController

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ApiRest.Controllers;
+
+[ApiController]
+public class HomeController : ControllerBase
+{
+    [HttpGet("/")]
+    public IActionResult Get() => Ok();
+}
---
+++ Controllers/VideoController.cs
@@ -0,0 +1,14 @@
+﻿using ApiRest.Data;
+using ApiRest.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiRest.Controllers;
+
+[ApiController]
+public class VideoController : ControllerBase
+{
+    [HttpGet("/videos")]
+    public async Task<IActionResult> Get([FromServices] VideoContext context) 
+        => Ok(await context.Videos.AsNoTracking().ToListAsync());
+}
